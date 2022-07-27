101ae6399e97fdd83edd7028cbbf5c18879bd1b5
Valya Vyrodova <valya.vyrodova@gmail.com>
2022-07-27 13:31:54 +0300

    added collision detection btw the ball and paddle

--- game_brick-breaker/src/ball.js
+++ game_brick-breaker/src/ball.js
@@ -5,6 +5,8 @@ class Ball {
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
 
+        this.game = game;
+
         this.speed = {x: 4, y:4};
         this.position = {x: 10, y: 10};
         this.size = 40;
@@ -20,13 +22,26 @@ class Ball {
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // wall on left or right
         if(this.position.x + this.size> this.gameWidth || this.position.x < 0) {
             this.speed.x = -this.speed.x;
         }
 
+        // wall on top or bottom 
         if(this.position.y + this.size > this.gameHeight|| this.position.y < 0) {
             this.speed.y = -this.speed.y;
         }
+
+        // check collision with paddle 
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+
+        if(bottomOfBall >= topOfPaddle) {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
+        }
+
+
 
     }
 }
