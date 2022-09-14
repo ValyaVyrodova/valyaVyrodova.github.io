d560ae1021c55c60bfe9398f04cd6fce3905e662
Valya Vyrodova <valya.vyrodova@gmail.com>
2022-09-14 17:03:14 +0300

    implement change color of rectangle

--- canvas_api/collision-detection/rectangular/canvas.js
+++ canvas_api/collision-detection/rectangular/canvas.js
@@ -32,73 +32,103 @@ addEventListener('resize', () => {
 })
 
 //Unity functions 
-function randomIntFromRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
+// function randomIntFromRange(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+// }
 
-function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)]
-}
+// function randomColor(colors) {
+//     return colors[Math.floor(Math.random() * colors.length)]
+// }
 
-function getDistance(x1, y1, x2, y2) {
-    let xDistance = x2 - x1
-    let yDistanse = y2 - y1
+// function getDistance(x1, y1, x2, y2) {
+//     let xDistance = x2 - x1
+//     let yDistanse = y2 - y1
 
-    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistanse, 2))
-}
+//     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistanse, 2))
+// }
 
 
 
 // Objects
-function Object(x, y, radius, color) {
+// function Object(x, y, radius, color) {
 
-    this.x = x;
-    this.y = y;
-    this.velocity = {
-        x: (Math.random() - 0.5) * 5,
-        y: (Math.random() - 0.5) * 5
-    };
-    this.radius = radius;
-    this.color = color;
+//     this.x = x;
+//     this.y = y;
+//     this.velocity = {
+//         x: (Math.random() - 0.5) * 5,
+//         y: (Math.random() - 0.5) * 5
+//     };
+//     this.radius = radius;
+//     this.color = color;
 
-    this.update = () => {
-        this.draw()
-    }
+//     this.update = () => {
+//         this.draw()
+//     }
 
-    this.draw = function() {
-        c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.fillStyle = this.color;
-        c.fill()
-        c.strokeStyle = this.color
-        c.stroke()
-        c.closePath()
-    }
+//     this.draw = function() {
+//         c.beginPath()
+//         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+//         c.fillStyle = this.color;
+//         c.fill()
+//         c.strokeStyle = this.color
+//         c.stroke()
+//         c.closePath()
+//     }
 
 
-}
+// }
 
 // Implementation
-let array
+// let array
 
-function init() {
-    array = []
+// function init() {
+//     array = []
 
-    for (let i = 0; i < 100; i++) {
+//     for (let i = 0; i < 100; i++) {
         
-        array.push(new Object(x, y, radius, color))
-    }
-}
+//         array.push(new Object(x, y, radius, color))
+//     }
+// }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    particles.forEach(particle => {
-        particle.update(particles)
-    })
+    c.fillStyle = '#1A1A23'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    // c.clearRect(0, 0, canvas.width, canvas.height)
+
+    const redRectangles = {
+        x: mouse.x,
+        y: mouse.y,
+        side: 100,
+        color: '#E86262'
+    }
+
+    const blueRectangles = {
+        x: canvas.width / 2 - 50,
+        y: canvas.height / 2 - 50,
+        side: 100,
+        color: '#92ABEA'
+    }
+
+    if(redRectangles.x + redRectangles.side >= blueRectangles.x && 
+        redRectangles.x <=  blueRectangles.x + blueRectangles.side && 
+        redRectangles.y + redRectangles.side >= blueRectangles.y && 
+        redRectangles.y  <= blueRectangles.y + blueRectangles.side ) {
+            blueRectangles.color = '#E86262'
+    } else {
+        blueRectangles.color = '#92ABEA'
+    }
+
+    //red rectangle
+    c.fillStyle = redRectangles.color
+    c.fillRect(redRectangles.x, redRectangles.y, redRectangles.side, redRectangles.side)
+
+    //blue rectangle
+    c.fillStyle = blueRectangles.color
+    c.fillRect(blueRectangles.x, blueRectangles.y, blueRectangles.side, blueRectangles.side)
 
 }
 
-init()
+
 animate()
